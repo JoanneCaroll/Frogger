9f1a1dfb0fcc16197a2ac6da57ae26efbc2584ef
JoanneCaroll <j.madrona@klab.com>
2015-01-05 10:56:38 +0800

    changed applicationadapter to application listener. and made badlogix to frog image.

--- core/src/com/mygdx/frogger/FroggerGame.java
+++ core/src/com/mygdx/frogger/FroggerGame.java
@@ -1,27 +1,45 @@
 package com.mygdx.frogger;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class FroggerGame extends ApplicationAdapter {
+public class FroggerGame implements ApplicationListener {
 	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+	Texture frog;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        frog = new Texture("sprite/frog.png");
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(frog, 10, 30);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+
 }
