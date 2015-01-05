508c0c1a6bf961d256c583a657fb7955816a08ab
JoanneCaroll <j.madrona@klab.com>
2015-01-05 16:35:34 +0800

    water object added and populated in screen

--- core/src/com/mygdx/frogger/screens/GameScreen.java
+++ core/src/com/mygdx/frogger/screens/GameScreen.java
@@ -11,6 +11,7 @@ import com.mygdx.frogger.com.mygdx.frogger.objects.Frog;
 import com.mygdx.frogger.com.mygdx.frogger.objects.GameObject;
 import com.mygdx.frogger.com.mygdx.frogger.objects.Grass;
 import com.mygdx.frogger.com.mygdx.frogger.objects.Road;
+import com.mygdx.frogger.com.mygdx.frogger.objects.Water;
 
 import java.util.ArrayList;
 
@@ -21,6 +22,7 @@ public class GameScreen implements Screen {
     private Frog frog;
     private ArrayList<GameObject> grass = new ArrayList<GameObject>();
     private ArrayList<GameObject> road = new ArrayList<GameObject>();
+    private ArrayList<GameObject> water = new ArrayList<GameObject>();
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private Frogger game;
@@ -39,11 +41,14 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         batch.begin();
         batch.setProjectionMatrix(camera.combined);
-        for (GameObject z : grass) {
-            z.draw(batch);
+        for (GameObject g : grass) {
+            g.draw(batch);
         }
-        for (GameObject z : road) {
-            z.draw(batch);
+        for (GameObject r : road) {
+            r.draw(batch);
+        }
+        for (GameObject w : water) {
+            w.draw(batch);
         }
         frog.draw(batch);
         batch.end();
@@ -92,6 +97,26 @@ public class GameScreen implements Screen {
             }
             for(int i=0; i<23; i++) {
                 road.add(new Road(px, max));
+                px += 32;
+            }
+        }
+
+        //adding of water
+        for(int g=0; g<3; g++) {
+            if(g==0){
+                px=0;
+                max = 192;
+            }
+            else if(g==1){
+                px=0;
+                max = 224;
+            }
+            else if(g==2){
+                px=0;
+                max = 256;
+            }
+            for(int i=0; i<23; i++) {
+                water.add(new Water(px, max));
                 px += 32;
             }
         }
