f74d35d5d6fafe4f28edc936baa2a52c549ae591
JoanneCaroll <j.madrona@klab.com>
2015-01-05 15:33:46 +0800

    made screen bigger

--- core/src/com/mygdx/frogger/screens/GameScreen.java
+++ core/src/com/mygdx/frogger/screens/GameScreen.java
@@ -2,6 +2,7 @@ package com.mygdx.frogger.screens;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.frogger.Frogger;
@@ -12,23 +13,28 @@ import com.mygdx.frogger.com.mygdx.frogger.objects.Frog;
  */
 public class GameScreen implements Screen {
     private Frog frog;
+    private OrthographicCamera camera;
     private SpriteBatch batch;
     private Frogger game;
     private BitmapFont font;
 
     public GameScreen(Frogger game){
         this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 720, 360);
         batch = new SpriteBatch();
         frog = new Frog();
-        frog.setPosition(10,30);
+        frog.setPosition(0,32);
         font = new BitmapFont();
         font.setScale(1.5f);
         font.setColor(Color.RED);
+        
     }
 
     @Override
     public void render(float delta) {
         batch.begin();
+        batch.setProjectionMatrix(camera.combined);
         frog.draw(batch);
         font.draw(batch, "Game Screen", 150, 120);
         batch.end();
